4231dbaba01f7d61f93a771d0ecd65d2f34472f8
Matteo Gildone <matteo.gildone@springernature.com>
2019-12-12 15:32:19 +0000

    first setup

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="App lg">
+      <div className="scanlines"></div>
+      <div className="scanline"></div>
+      <div className="flicker"></div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
