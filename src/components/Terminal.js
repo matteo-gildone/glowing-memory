bbb50eebd84cae5c5279492683b8c3c80c5e5d0c
Matteo Gildone <matteo.gildone@springernature.com>
2020-01-10 10:58:38 +0000

    fix #2

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import { Cmd } from "./Cmd";
 import { Input } from "./Input";
 import { Result } from "./Result";
 import { List } from "./List";
+import { useTerminalState } from "./TerminalProvider";
 const Terminal = ({ children }) => {
+  const htmlRef = useRef(null);
+  const { commands } = useTerminalState();
+  useEffect(() => {
+    htmlRef.current.lastChild.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest"
+    });
+  }, [commands]);
   return (
-    <div className="c-terminal">
+    <div ref={htmlRef} className="c-terminal">
       <div className="u-scanlines"></div>
       <div className="u-scanline"></div>
       <div className="u-flicker"></div>
